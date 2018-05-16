3f466cb02132053b83860ce232737bab4e0224ce
Théo <tmarty@teclib.com>
2018-05-16 13:29:23 +0200

    [ADD] Add interface with layout

--- app/src/main/java/com/example/theo/dlcapp/MainActivity.java
+++ app/src/main/java/com/example/theo/dlcapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.theo.dlcapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_connexion = (Button) findViewById(R.id.connexion);
+        button_connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //navigate to other activity
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
